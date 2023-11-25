f0caae23959e74929b270c123abeabe7505d830c
Thành Phúc <lephucthanh1210@gmail.com>
2023-11-25 18:36:52 +0700

    Update

--- Day5/main.js
+++ Day5/main.js
@@ -67,5 +67,48 @@ var languages = [
     'PHP',
     'Ruby',
 ];
-// Xóa element lần cuối vả trả về phần tử đã xóa 
 console.log(languages.shift())
+
+//-Unshift
+//Thêm 1 hoặc nhiều phần tử vào đầ mảng
+var languages = [
+    'JavaScript',
+    'PHP',
+    'Ruby',
+];
+ 
+console.log(languages.unshift('Thanh','Phuc'))
+
+//Splicing
+// có thể cắt hoặc chèn phần tử mới vào mảng
+var languages = [
+    'JavaScript',
+    'PHP',
+    'Ruby',
+];
+//( vị trí đặt con trỏ và đích số muốn xóa) hoặc có thể chèn vào bằng tham số thứ 3
+languages.splice(1,0 , 'Dart')
+console.log(languages)
+
+//-Concat
+//Giúp nối ARRAY
+var languages = [
+    'JavaScript',
+    'PHP',
+    'Ruby',
+];
+var languages2 = [
+    'Ruby',
+    'Java',
+    'RTB',
+];
+console.log(languages.concat(languages2))
+
+//slicing
+//Cắt
+var languages = [
+    'JavaScript',
+    'PHP',
+    'Ruby',
+];
+console.log(languages.slice(1, 2))
